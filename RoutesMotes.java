ccd00cee15f93fdd620d62bb5efa9a6edb031f42
Marlon W. Santos <marlon.santos.santos@icen.ufpa.br>
2020-01-18 10:51:44 -0300

    Adicionado RoutesMotes.java

---
+++ RoutesMotes.java
@@ -0,0 +1,61 @@
+
+/*     Routes Motes - It filter and store the routes and IPS of the motes
+*      Copyright (c) 2020 Marlon W. Santos <marlon.santos.santos@icen.ufpa.br>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>5.
+*
+*/
+
+
+public class RoutesMotes{
+
+    //Armazena as informações dos IPs e das rotas dos motes
+  public static void SetResponse(String[] args){
+
+  }
+
+
+    //Filtra e elimina os ruídos dos IPs e das Rotas
+  public static void FilterResponse(){
+
+  }
+
+    //Armazena o IP do mote
+  public static void SetIp(){
+
+  }
+
+    //Armazena a rota do mote 
+  public static void SetRoute(){
+
+  }
+
+    //Retorna o IP do mote
+  public static String GetIP(){
+
+  }
+
+    //Retorna a rota do mote
+  public static String GetRoute(){
+
+  }
+
+
+
+  public static main(String[] args){
+
+
+  }
+
+}
